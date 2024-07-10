5c3f2cf54bc23cea997ae94b73b6e8bf2527c229
shofiya2003 <shofiyabootwala@gmail.com>
2024-07-10 17:55:49 +0530

    fix : handled AttributeError for query api

--- service/src/controllers/game_controllers.py
+++ service/src/controllers/game_controllers.py
@@ -14,52 +14,63 @@ session = Session(engine)
 @game.route('/', methods=['GET'])
 @jwt_required()
 def query():
-    params = request.args
-    query = session.query(Game)
-    aggregate_results = {}
-    for key, value in params.items():
-        if hasattr(Game, key):
-            column = getattr(Game, key)
-            if column.type.python_type == int or column.type.python_type == float:
-                if value.startswith('>'):
-                    query = query.filter(column > float(value[1:]))
-                elif value.startswith('<'):
-                    query = query.filter(column < float(value[1:]))
+    try:
+        params = request.args
+        query = session.query(Game)
+        aggregate_results = {}
+        for key, value in params.items():
+            if hasattr(Game, key):
+                column = getattr(Game, key)
+                if column.type.python_type == int or column.type.python_type == float:
+                    if value.startswith('>'):
+                        query = query.filter(column > float(value[1:]))
+                    elif value.startswith('<'):
+                        query = query.filter(column < float(value[1:]))
+                    else:
+                        query = query.filter(column == float(value))
+                elif column.type.python_type == bool:
+                    query = query.filter(column == (value.lower() == 'true'))
+                elif column.type.python_type == datetime.date:
+                    if value.startswith('>'):
+                        query = query.filter(column > datetime.datetime.strptime(value[1:], '%Y-%m-%d').date())
+                    elif value.startswith('<'):
+                        query = query.filter(column < datetime.datetime.strptime(value[1:], '%Y-%m-%d').date())
+                    else:
+                        query = query.filter(column == datetime.datetime.strptime(value, '%Y-%m-%d').date())
                 else:
-                    query = query.filter(column == float(value))
-            elif column.type.python_type == bool:
-                query = query.filter(column == (value.lower() == 'true'))
-            elif column.type.python_type == datetime.date:
-                if value.startswith('>'):
-                    query = query.filter(column > datetime.datetime.strptime(value[1:], '%Y-%m-%d').date())
-                elif value.startswith('<'):
-                    query = query.filter(column < datetime.datetime.strptime(value[1:], '%Y-%m-%d').date())
+                    if key=="name":
+                        print(value)
+                        query = query.filter(column.like(f'{value}%'))
+                        continue
+                    query = query.filter(column.like(f'%{value}%'))
+    
+            elif key in ['sum', 'max', 'min', 'avg']:
+                print(value)
+                if hasattr(Game, value):
+                    func_column = getattr(Game, value)
+                    if func_column.type.python_type == int or func_column.type.python_type == float:
+                        if key == 'sum':
+                            result = session.query(func.sum(func_column)).scalar()
+                        elif key == 'max':
+                            result = session.query(func.max(func_column)).scalar()
+                        elif key == 'min':
+                            result = session.query(func.min(func_column)).scalar()
+                        elif key == 'avg':
+                            result = session.query(func.avg(func_column)).scalar()
+                        aggregate_results[f"{key}_{func_column}"] = result
+                    else:
+                        return json.jsonify({"msg": "Aggrgate function can be applied to only Integer or Float columns "}) , 400
                 else:
-                    query = query.filter(column == datetime.datetime.strptime(value, '%Y-%m-%d').date())
+                    return json.jsonify({"msg": f"Attribute {value} not present"}), 400
             else:
-                if key=="name":
-                    print(value)
-                    query = query.filter(column.like(f'{value}%'))
-                    continue
-                query = query.filter(column.like(f'%{value}%'))
-
-        elif key in ['sum', 'max', 'min', 'avg']:
-            func_column = getattr(Game, value)
-            if func_column.type.python_type == int or func_column.type.python_type == float:
-                if key == 'sum':
-                    result = session.query(func.sum(func_column)).scalar()
-                elif key == 'max':
-                    result = session.query(func.max(func_column)).scalar()
-                elif key == 'min':
-                    result = session.query(func.min(func_column)).scalar()
-                elif key == 'avg':
-                    result = session.query(func.avg(func_column)).scalar()
-                aggregate_results[f"{key}_{func_column}"] = result
-            else:
-                return json.jsonify({"msg":"Aggrgate function can be applied to only Integer or Float columns "}) , 400
-            
-    results = query.all()
-    if len(aggregate_results.keys()) != 0:
-        return json.jsonify({"games":[game.to_dict() for game in results],"aggregate_results":aggregate_results}) , 200
-    else:
-        return json.jsonify({"games":[game.to_dict() for game in results]}) , 200
+                return json.jsonify({"msg": f"Attribute {key} not present"}), 400
+                
+        results = query.all()
+        if len(aggregate_results.keys()) != 0:
+            return json.jsonify({"games":[game.to_dict() for game in results],"aggregate_results":aggregate_results}) , 200
+        else:
+            return json.jsonify({"games":[game.to_dict() for game in results]}) , 200
+    except AttributeError as e:
+        return json.jsonify({"msg":"Attribute missing in "})
+    except Exception as e:
+        return json.jsonify({"msg": "Something went wrong"}), 500
